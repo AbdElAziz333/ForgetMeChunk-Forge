0ceb00369f57b72d0e8a748338846de84d0c3a2c
AbdElAziz333 <89092953+AbdElAziz333@users.noreply.github.com>
2022-07-10 10:41:07 +0200

    fix the mod doesn't work

--- src/main/java/com/abdelaziz333/forgetmechunk/ForgetMeChunk.java
+++ src/main/java/com/abdelaziz333/forgetmechunk/ForgetMeChunk.java
@@ -1,11 +1,15 @@
 package com.abdelaziz333.forgetmechunk;
 
+//import com.abdelaziz333.forgetmechunk.util.MixinExtrasBootstrap;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod("forgetmechunk")
 public class ForgetMeChunk {
-
     public static final Logger LOGGER = LogManager.getLogger();
+
+    /* public ForgetMeChunk () {
+        MixinExtrasBootstrap.init();
+    } */
 }
--- src/main/java/com/abdelaziz333/forgetmechunk/util/WrapWithCondition.java
+++ src/main/java/com/abdelaziz333/forgetmechunk/util/WrapWithCondition.java
@@ -1,10 +1,16 @@
 package com.abdelaziz333.forgetmechunk.util;
 
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Slice;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface WrapWithCondition {
-
     String[] method();
-
-    At at();
+    At[] at();
 }
