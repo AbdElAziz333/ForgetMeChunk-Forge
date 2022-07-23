5c017964748f3e9013d876e5f72674b1f049f98c
AbdElAziz333 <89092953+AbdElAziz333@users.noreply.github.com>
2022-07-23 11:29:02 +0200

    reformat coding

---
+++ src/main/java/com/abdelaziz/forgetmechunk/ForgetMeChunk.java
@@ -0,0 +1,16 @@
+package com.abdelaziz.forgetmechunk;
+
+//import com.abdelaziz333.forgetmechunk.util.MixinExtrasBootstrap;
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("forgetmechunk")
+public class ForgetMeChunk {
+    public static final Logger LOGGER = LogManager.getLogger();
+/*
+    public ForgetMeChunk () {
+        MixinExtrasBootstrap.init();
+    }
+    */
+}
---
+++ src/main/java/com/abdelaziz/forgetmechunk/util/WrapWithCondition.java
@@ -0,0 +1,16 @@
+package com.abdelaziz.forgetmechunk.util;
+
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface WrapWithCondition {
+    String[] method();
+
+    At[] at();
+}
