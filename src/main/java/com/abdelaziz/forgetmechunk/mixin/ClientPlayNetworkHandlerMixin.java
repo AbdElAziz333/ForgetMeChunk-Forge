6792b89d443f96c4ac7644b72d0fc06d6f06c631
AbdElAziz333 <89092953+AbdElAziz333@users.noreply.github.com>
2022-10-23 02:01:39 +0200

    change: replace mixin extras with redirect

--- src/main/java/com/abdelaziz/forgetmechunk/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/com/abdelaziz/forgetmechunk/mixin/ClientPlayNetworkHandlerMixin.java
@@ -1,16 +1,16 @@
 package com.abdelaziz.forgetmechunk.mixin;
 
-import com.abdelaziz.forgetmechunk.util.WrapWithCondition;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.util.math.ChunkSectionPos;
 import net.minecraft.world.chunk.light.LightingProvider;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
-    @WrapWithCondition(method = "method_38546", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/chunk/light/LightingProvider;setSectionStatus(Lnet/minecraft/util/math/ChunkSectionPos;Z)V"))
-    private boolean shouldDoUselessLightUpdate(LightingProvider instance, ChunkSectionPos pos, boolean notReady) {
-        return false;
+    @Redirect(method = "method_38546", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/chunk/light/LightingProvider;setSectionStatus(Lnet/minecraft/util/math/ChunkSectionPos;Z)V"))
+    private void injected(LightingProvider instance, ChunkSectionPos pos, boolean notReady) {
+
     }
 }
