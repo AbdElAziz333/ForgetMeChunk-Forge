9901468c6ae2ea5fe852216b3bf72d7edd788a61
AbdElAziz333 <89092953+AbdElAziz333@users.noreply.github.com>
2022-10-23 02:01:08 +0200

    change: update for 1.19.2.

--- src/main/java/com/abdelaziz/forgetmechunk/ForgetMeChunk.java
+++ src/main/java/com/abdelaziz/forgetmechunk/ForgetMeChunk.java
@@ -1,16 +1,11 @@
 package com.abdelaziz.forgetmechunk;
 
-//import com.abdelaziz333.forgetmechunk.util.MixinExtrasBootstrap;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod("forgetmechunk")
+@Mod(ForgetMeChunk.MOD_ID)
 public class ForgetMeChunk {
+    public static final String MOD_ID = "forgetmechunk";
     public static final Logger LOGGER = LogManager.getLogger();
-    /*
-    public ForgetMeChunk() {
-        MixinExtrasBootstrap.init();
-    }
-    */
 }
