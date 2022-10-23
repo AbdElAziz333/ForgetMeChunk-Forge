ba0b9ed4eae885f1e420a883e9ceb50f6449d1a2
AbdElAziz333 <89092953+AbdElAziz333@users.noreply.github.com>
2022-10-23 03:44:57 +0200

    change: replaced WrapWithCondition with Redirect annotation

--- src/main/java/com/abdelaziz/forgetmechunk/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/com/abdelaziz/forgetmechunk/mixin/ClientPlayNetworkHandlerMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.util.math.ChunkSectionPos;
 import net.minecraft.world.chunk.light.LightingProvider;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
